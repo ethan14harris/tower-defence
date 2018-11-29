39b8a888df014e0d8fd63dfaa7bf3a0d5616d5dc
SUSSEXDOWNS\475039 <475039@chrome.sussexdowns.ac.uk>
2018-11-29 14:36:06 +0000

    done bits wooooooooooooo

---
+++ Assets/enemymovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class enemymovement : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void start()
+    {
+        points = new Transform[transform.childCDount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+    {
+        target = Waypoint.points{0};
+    }
+
+    void update ()
+    {
+        vector3 dir = target.position - transform.position;
+         transform.Translate(dir.normalized * speed * Time.deltaTime, space.wor);
+
+
+
+    }   
+
+}
---
+++ Assets/waypoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class waypoints : MonoBehaviour {
+
+    public static Transform[] points;
+
+    void awake ()
+    {
+        points = new Transform[transform.childCDount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+	
+}
