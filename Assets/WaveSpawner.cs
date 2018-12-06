e7106714d4070fd998f1f4689d461f28fab8d05f
SUSSEXDOWNS\475039 <475039@chrome.sussexdowns.ac.uk>
2018-12-06 14:30:53 +0000

    started a wave spawner

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public Transform enemyprefab;
+
+    public float timeBetweenWaves = 5f;
+    public float countdown = 2f;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+    void SpawnWave()
+    {
+        Debug.Log("Wave Incoming!");
+    }
+
+}
